264d594aff4dc210e9f65a77b4080512a3d6f556
ResadOzbas <75367333+ResadOzbas@users.noreply.github.com>
2023-04-18 16:26:31 +0100

    Start of new project, 16x16 grid

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i<4; i++){
+    const divi = document.createElement('div')
+    container.appendChild(divi);
+    for (let y = 0; y<4; y++){
+        const divy = document.createElement
+        divi.appendChild(divy);
+    } 
+}
