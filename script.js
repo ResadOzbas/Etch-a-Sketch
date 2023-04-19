1e2dcf7b60e488cd073820f20220e27d10e1e457
ResadOzbas <75367333+ResadOzbas@users.noreply.github.com>
2023-04-19 18:38:02 +0100

    created the 16x16 grid

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const container = document.querySelector('.container');
 
-for (let i = 0; i<4; i++){
-    const divi = document.createElement('div')
-    container.appendChild(divi);
-    for (let y = 0; y<4; y++){
-        const divy = document.createElement
-        divi.appendChild(divy);
-    } 
-}
+document.addEventListener('DOMContentLoaded', function(){
+    const container = document.querySelector('.container');
+   
+    for (let i = 0; i<4; i++){
+        const divi = document.createElement('div')
+        divi.classList.add('flex-container')
+        container.appendChild(divi);
+        for (let y = 0; y<4; y++){
+            const divy = document.createElement('div');
+            divy.textContent = 'hi';
+            divi.appendChild(divy);
+        } 
+    }
+});
