450212d8cb83e2baf981508985b32ac1c930c298
ResadOzbas <75367333+ResadOzbas@users.noreply.github.com>
2023-04-20 17:29:09 +0100

    increased to 16x16 and added hovering aspect, 80% complete

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-
+//ensures DOM loads before script runs
 document.addEventListener('DOMContentLoaded', function(){
     const container = document.querySelector('.container');
    
-    for (let i = 0; i<4; i++){
+    //creates the 16x16 grid, first makes 16 rows, then 16 divs in those rows 
+    for (let i = 0; i<16; i++){
         const divi = document.createElement('div')
         divi.classList.add('flex-container')
         container.appendChild(divi);
-        for (let y = 0; y<4; y++){
+        for (let y = 0; y<16; y++){
             const divy = document.createElement('div');
-            divy.textContent = 'hi';
             divi.appendChild(divy);
         } 
     }
+    const gridDiv = document.querySelectorAll('.flex-container div');
+    
+    //Event listener for hovoring grid, change colour
+    gridDiv.forEach(function(div){
+        div.addEventListener('mouseenter', function(){
+            div.classList.add('hovered');
+        })
+    })
+    
+    gridDiv.forEach(function(div){
+        div.addEventListener('mouseleave', function(){
+            div.classList.remove('hovered');
+        })
+    })
+
 });
